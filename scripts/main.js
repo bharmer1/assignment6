3cc79d50e192abd7ab98a19b3b2a48d60e2d17dc
Adam <adammonast@Adams-MBP.attlocal.net>
2020-04-26 18:00:38 -0400

    created members array, tour object, while loop

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+// Members Array
+const members = [
+  "Dave Ghrohl",
+  "Nate Mendel",
+  "Pat Smear",
+  "Taylor Hawkins",
+  "Rami Jaffee",
+  "Chris Shiflett",
+];
+
+// Tour Object
+const tour = {
+  chicago: "July 29th and 30th, 2018",
+  seattle: "September 1st, 2018",
+  edmonton: "September 4th, 2018",
+};
+
+// While Loop
+var x = 0;
+while (x < 5) {
+  console.log("Times running through the loop x");
+  x++;
+}
